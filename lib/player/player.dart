ae93e02233fec5ac04025546be24597391f10bfc
Vadim <44135514+vodemn@users.noreply.github.com>
2020-08-07 12:40:41 +0300

    master without vlc

--- lib/player/player.dart
+++ lib/player/player.dart
@@ -1,108 +1,101 @@
 import 'dart:async';
 
-import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:flutter_fastotv_common/player/iplayer.dart';
-import 'package:screen/screen.dart';
+import 'package:video_player/video_player.dart';
 
-abstract class IPlayerState {}
+class FlutterPlayer extends IPlayer {
+  VideoPlayerController _controller;
 
-class InitIPlayerState extends IPlayerState {}
-
-class ErrorState extends IPlayerState {}
-
-class ReadyToPlayState extends IPlayerState {
-  ReadyToPlayState(this.url, this.userData);
-
-  final String url;
-  final dynamic userData;
-}
-
-abstract class LitePlayer<T extends StatefulWidget, S> extends State<T> {
-  static const TS_DURATION_MSEC = 5000;
-  IPlayer get player;
-  final StreamController<IPlayerState> state = StreamController<IPlayerState>.broadcast();
-
-  LitePlayer();
-
-  void playLink(String url, dynamic userData);
-
-  void onPlaying(dynamic userData);
-
-  void onPlayingError(dynamic userData);
-
-  void seekToInterrupt() {}
-
-  String currentUrl();
-
-  void playChannel(S stream);
-
-  bool isPlaying() {
-    return player.isPlaying();
-  }
-
-  Duration position() {
-    return player.position();
-  }
-
-  Future<void> pause() async {
-    return player.pause();
-  }
-
-  Future<void> play() async {
-    return player.play();
-  }
-
-  Future<void> seekTo(Duration duration) async {
-    return player.seekTo(duration);
-  }
-
-  Future<void> seekForward(Duration duration) {
-    return player.seekForward(duration);
-  }
-
-  Future<void> seekBackward(Duration duration) {
-    return player.seekBackward(duration);
-  }
-
-  Future<void> setVolume(double volume) async {
-    return player.setVolume(volume);
+  @override
+  Widget timeLine() {
+    return VideoProgressIndicator(_controller, allowScrubbing: true);
   }
 
   @override
-  void initState() {
-    _setScreen(true);
-    super.initState();
+  Widget makePlayer() {
+    return AspectRatio(aspectRatio: aspectRatio(), child: VideoPlayer(_controller));
+  }
+
+  @override
+  bool isPlaying() {
+    if (_controller == null) {
+      return false;
+    }
+
+    return _controller.value.isPlaying;
+  }
+
+  @override
+  Duration position() {
+    if (_controller == null) {
+      return Duration(milliseconds: 0);
+    }
+    return _controller.value.position;
+  }
+
+  @override
+  double aspectRatio() {
+    if (_controller == null) {
+      return 16 / 9;
+    }
+
+    return _controller.value.aspectRatio;
+  }
+
+  @override
+  Future<void> pause() async {
+    if (_controller == null) {
+      return Future.error('Invalid state');
+    }
+
+    return _controller.pause();
+  }
+
+  @override
+  Future<void> play() async {
+    if (_controller == null) {
+      return Future.error('Invalid state');
+    }
+
+    return _controller.play();
+  }
+
+  @override
+  Future<void> seekTo(Duration duration) async {
+    if (_controller == null) {
+      return Future.error('Invalid state');
+    }
+
+    return _controller.seekTo(duration);
+  }
+
+  @override
+  Future<void> setVolume(double volume) {
+    if (_controller == null) {
+      return Future.error('Invalid state');
+    }
+    return _controller.setVolume(volume);
+  }
+
+  @override
+  Future<void> setStreamUrl(Uri url) async {
+    if (url == null) {
+      return Future.error('Invalid input');
+    }
+
+    VideoPlayerController old = _controller;
+    _controller = VideoPlayerController.network(url.toString());
+    if (old != null) {
+      Future.delayed(Duration(milliseconds: 100)).then((_) {
+        old.dispose();
+      });
+    }
+    return _controller.initialize();
   }
 
   @override
   void dispose() {
-    state.close();
-    _setScreen(false);
-    player.dispose();
-    super.dispose();
-  }
-
-  Widget timeLine() {
-    return StreamBuilder<IPlayerState>(
-        stream: state.stream,
-        initialData: InitIPlayerState(),
-        builder: (context, snapshot) {
-          if (snapshot.data is ReadyToPlayState) {
-            return player.timeLine();
-          }
-          return player.makeLinear();
-        });
-  }
-
-  // private:
-  void changeState(IPlayerState state) {
-    this.state.add(state);
-  }
-
-  void _setScreen(bool keepOn) {
-    if (!kIsWeb) {
-      Screen.keepOn(keepOn);
-    }
+    _controller?.dispose();
   }
 }
---
+++ lib/player/widget.dart
@@ -0,0 +1,174 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:flutter/material.dart';
+import 'package:flutter_fastotv_common/player/player.dart';
+import 'package:screen/screen.dart';
+
+abstract class IPlayerState {}
+
+class InitIPlayerState extends IPlayerState {}
+
+class ErrorState extends IPlayerState {}
+
+class ReadyToPlayState extends IPlayerState {
+  ReadyToPlayState(this.url, this.userData);
+
+  final String url;
+  final dynamic userData;
+}
+
+abstract class LitePlayer<T extends StatefulWidget, S> extends State<T> {
+  static const TS_DURATION_MSEC = 5000;
+  final StreamController<IPlayerState> state = StreamController<IPlayerState>.broadcast();
+
+  FlutterPlayer _player = FlutterPlayer();
+
+  FlutterPlayer get player => _player;
+
+  void playLink(String url, dynamic userData) {
+    if (url.isEmpty) {
+      return;
+    }
+
+    _initLink(url, (uri) => _setVideoLink(uri, userData));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.black,
+        child: Center(
+            child: StreamBuilder<IPlayerState>(
+                stream: state.stream,
+                initialData: InitIPlayerState(),
+                builder: (context, snapshot) {
+                  if (snapshot.data is ReadyToPlayState) {
+                    seekToInterrupt();
+                    return _player.makePlayer();
+                  }
+                  return _player.makeCircular();
+                })));
+  }
+
+  LitePlayer();
+
+  void onPlaying(dynamic userData);
+
+  void onPlayingError(dynamic userData);
+
+  void seekToInterrupt() {}
+
+  String currentUrl();
+
+  void playChannel(S stream);
+
+  bool isPlaying() {
+    return player.isPlaying();
+  }
+
+  Duration position() {
+    return player.position();
+  }
+
+  Future<void> pause() async {
+    return player.pause();
+  }
+
+  Future<void> play() async {
+    return player.play();
+  }
+
+  Future<void> seekTo(Duration duration) async {
+    return player.seekTo(duration);
+  }
+
+  Future<void> seekForward(Duration duration) {
+    return player.seekForward(duration);
+  }
+
+  Future<void> seekBackward(Duration duration) {
+    return player.seekBackward(duration);
+  }
+
+  Future<void> setVolume(double volume) async {
+    return player.setVolume(volume);
+  }
+
+  @override
+  void initState() {
+    _initLink(currentUrl(), (uri) => _setVideoLink(uri, null));
+    _setScreen(true);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    state.close();
+    _setScreen(false);
+    player.dispose();
+    super.dispose();
+  }
+
+  Widget timeLine() {
+    return StreamBuilder<IPlayerState>(
+        stream: state.stream,
+        initialData: InitIPlayerState(),
+        builder: (context, snapshot) {
+          if (snapshot.data is ReadyToPlayState) {
+            return player.timeLine();
+          }
+          return player.makeLinear();
+        });
+  }
+
+  // private:
+  void _changeState(IPlayerState state) {
+    this.state.add(state);
+  }
+
+  void _setScreen(bool keepOn) {
+    if (!kIsWeb) {
+      Screen.keepOn(keepOn);
+    }
+  }
+
+  void _initLink(String url, void Function(Uri) onInit) async {
+    final parsed = Uri.tryParse(url);
+    if (parsed == null) {
+      return;
+    }
+
+    _changeState(InitIPlayerState());
+    /*if (parsed.scheme == 'http' || parsed.scheme == 'https') {
+      try {
+        final resp = await http.get(url).timeout(const Duration(seconds: 1));
+        if (resp.statusCode == 202) {
+          Future.delayed(Duration(milliseconds: TS_DURATION_MSEC)).whenComplete(() {
+            onInit(parsed);
+          });
+        } else {
+          onInit(parsed);
+        }
+      } on TimeoutException catch (e) {
+        onInit(parsed);
+      } on Error catch (e) {
+        onInit(parsed);
+      }
+      return;
+    }*/
+
+    onInit(parsed);
+  }
+
+  void _setVideoLink(Uri url, dynamic userData) {
+    final Future<void> init = _player.setStreamUrl(url);
+    init.then((value) {
+      _changeState(ReadyToPlayState(url.toString(), userData));
+      play().then((_) {
+        onPlaying(userData);
+      }).catchError(() => onPlayingError(userData));
+    }).catchError(() => onPlayingError(userData));
+  }
+
+}
